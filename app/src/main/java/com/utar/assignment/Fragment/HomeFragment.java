ec25b083c1af7adadc3b4bd2db1a1bd33d5ab00b
JLeong0220 <lokjunleong2000@gmail.com>
2021-07-31 13:21:01 +0800

    new AddExpenses activity and add splithelper function

--- app/src/main/java/com/utar/assignment/Fragment/HomeFragment.java
+++ app/src/main/java/com/utar/assignment/Fragment/HomeFragment.java
@@ -14,20 +14,27 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.utar.assignment.Activity.AddExpenses;
 import com.utar.assignment.R;
 import com.utar.assignment.Util.GeneralHelper;
+import com.utar.assignment.Util.SplitCalHelper;
 
 public class HomeFragment  extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
+        SplitCalHelper sh = new SplitCalHelper();
+        double result = sh.splitNormal(5,5000);
+
         FloatingActionButton fab = view.findViewById(R.id.btnAddPost);
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //GeneralHelper.showMessage(getActivity(),"add expenses");
+
+                GeneralHelper.showMessage(getActivity(),"answer test is " +result);
+
                 Intent intent = new Intent(getActivity(), AddExpenses.class);
                 startActivity(intent);
 
