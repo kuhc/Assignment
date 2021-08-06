0d25b5e3934585adba01897594089ac0374fcde1
BQSIAH <siahbq7553@gmail.com>
2021-08-06 16:13:32 +0800

    Friend Fragment

--- app/src/main/java/com/utar/assignment/Activity/AddFriend.java
+++ app/src/main/java/com/utar/assignment/Activity/AddFriend.java
@@ -2,6 +2,9 @@ package com.utar.assignment.Activity;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,26 +13,25 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.SignInMethodQueryResult;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.utar.assignment.Model.Friend;
 import com.utar.assignment.Model.User;
 import com.utar.assignment.R;
 import com.utar.assignment.Util.FirebaseCallback;
 import com.utar.assignment.Util.FirestoreHelper;
 import com.utar.assignment.Util.GeneralHelper;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class AddFriend extends AppCompatActivity {
 
     private static FirebaseFirestore fStore = FirebaseFirestore.getInstance();
     private FirebaseAuth fAuth = FirebaseAuth.getInstance();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,10 +41,21 @@ public class AddFriend extends AppCompatActivity {
         EditText userEmail = findViewById(R.id.friendEmail);
         Button conformAddFriend = findViewById(R.id.conformAddFriend);
 
-
+        List<String> emailList = new ArrayList<>();
         FirebaseUser user;
         user = fAuth.getInstance().getCurrentUser();
         String uid = user.getUid();
+
+        FirestoreHelper.getUser(uid, new FirebaseCallback() {
+            @Override
+            public void onResponse(Object object) {
+                User userInfo;
+                userInfo = (User)object;
+                List<String> existingFriend = userInfo.getFriendList();
+                emailList.addAll(existingFriend);
+            }
+        });
+
         conformAddFriend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -58,21 +71,30 @@ public class AddFriend extends AppCompatActivity {
                                 userEmail.getText().clear();
                             } else {
                                 //GeneralHelper.showMessage(AddFriend.this, "The email does exist");
-                                List<String> emailList = null;
-                                emailList.add(email);
-                                Friend friend = new Friend();
-                                friend.setId(uid);
-                                friend.setFriendEmail(emailList);
 
-                               FirestoreHelper.addFriend(friend, new FirebaseCallback() {
-                                    @Override
-                                    public void onResponse() {
-                                        GeneralHelper.showMessage(AddFriend.this, "Successfully registered!");
-                                        //progressBar.setVisibility(View.INVISIBLE);
-                                        //btnRegister.setVisibility(View.VISIBLE);
-                                    }
-                                });
-                                userEmail.getText().clear();
+                                if (emailList.contains(email))
+                                {
+                                    GeneralHelper.showMessage(AddFriend.this, "The email is already in the friends list!");
+                                    userEmail.getText().clear();
+                                }
+                                else {
+                                    emailList.add(email);
+
+                                    FirestoreHelper.addFriend(uid, emailList, new FirebaseCallback() {
+                                        @Override
+                                        public void onResponse() {
+                                            GeneralHelper.showMessage(AddFriend.this, "Successfully registered!");
+
+                                            Intent intent = new Intent(AddFriend.this,MainActivity.class);
+                                            startActivity(intent);
+                                        }
+                                    });
+                                    userEmail.getText().clear();
+                                /*Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.friendList);
+                                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                                fragmentTransaction.detach(fragment).commit();
+                                fragmentTransaction.attach(fragment).commit();*/
+                                }
                             }
                         }
                     });
@@ -81,5 +103,6 @@ public class AddFriend extends AppCompatActivity {
                     GeneralHelper.showMessage(AddFriend.this, "Please type in the email");
             }
         });
+
     }
 }
--- app/src/main/java/com/utar/assignment/Fragment/FriendFragment.java
+++ app/src/main/java/com/utar/assignment/Fragment/FriendFragment.java
@@ -2,43 +2,57 @@ package com.utar.assignment.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-
+import android.telephony.mbms.MbmsErrors;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.utar.assignment.Activity.AddFriend;
 import com.utar.assignment.Model.User;
 import com.utar.assignment.R;
-import com.utar.assignment.Util.FirebaseCallback;
-import com.utar.assignment.Util.FirestoreHelper;
 import com.utar.assignment.Util.GeneralHelper;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class FriendFragment extends Fragment {
 
     private static FirebaseFirestore fStore = FirebaseFirestore.getInstance();
     private FirebaseAuth Auth;
+    FirebaseFirestore db;
+    RecyclerView recyclerView;
+    private User userInfo;
     TextView username;
+    TextView friendNo;
+    List<String> userList = new ArrayList<>();
+    View view;
+    ListView listView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_friend, container, false);
+        view = inflater.inflate(R.layout.fragment_friend, container, false);
+
+        username = view.findViewById(R.id.friend_frag_uname);
+        friendNo = view.findViewById(R.id.friend_no);
 
         FirebaseUser user;
         user = Auth.getInstance().getCurrentUser();
@@ -51,7 +65,7 @@ public class FriendFragment extends Fragment {
                         User user1 = task.getResult().toObject(User.class);
                         String um = user1.getUsername();
 
-                        username = view.findViewById(R.id.friend_frag_uname);
+
                         username.setText(um);
                     }
                 });
@@ -65,6 +79,68 @@ public class FriendFragment extends Fragment {
             }
         });
 
-       return view;
+       /* FirestoreHelper.getUser(uid, new FirebaseCallback() {
+            @Override
+            public void onResponse(Object object) {
+
+
+                recyclerView = view.findViewById(R.id.freindListRecycle);
+                userInfo = (User)object;
+                userList.addAll(userInfo.getFriendList());
+                int fNo = userList.size();
+                String fNo1 = Integer.toString(fNo);
+                friendNo.setText(fNo1);
+            }
+        });*/
+
+        db = FirebaseFirestore.getInstance();
+        DocumentReference documentReference =db.collection("Users").document(uid);
+        documentReference.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                userInfo = documentSnapshot.toObject(User.class);
+                userList.addAll(userInfo.getFriendList());
+                int fNo = userList.size();
+                String fNo1 = Integer.toString(fNo);
+                friendNo.setText(fNo1);
+
+                initializeListView (userInfo,userList);
+                //initializeRecycleView(userList);
+            }
+        });
+
+        return view;
     }
+
+    public void initializeListView(User userInfo, List<String> userList) {
+        listView = view.findViewById(R.id.friendList);
+        ArrayAdapter arrayAdapter;
+        ArrayList<String> email = new ArrayList<>(userList);
+        arrayAdapter = new ArrayAdapter(getContext(), android.R.layout.simple_list_item_1, email);
+        listView.setAdapter(arrayAdapter);
+        arrayAdapter.notifyDataSetChanged();
+
+         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            GeneralHelper.showMessage(getContext(),"This is friend no : " + email.get(position));
+        }
+    });
+}
+
+    /*private void initializeRecycleView(List<String> userList) {
+
+        LinearLayoutManager layoutManager =
+                //new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL,false);
+                new LinearLayoutManager(getContext());
+
+        recyclerView = (RecyclerView) view.findViewById(R.id.freindList);
+        recyclerView.setLayoutManager(layoutManager);
+
+        adapter = new friendListAdapter(getContext(),userList);
+        recyclerView.setAdapter(adapter);
+        GeneralHelper.showMessage(getContext(),"The no of friend: " + userList);
+
+    }*/
+
 }
