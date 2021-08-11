0594d1a31373c516021469a8307a9b62f882c53a
kuhc <kuhengchun0114@gmail.com>
2021-08-12 01:07:49 +0800

    add split menu

--- app/src/main/java/com/utar/assignment/Activity/MainActivity.java
+++ app/src/main/java/com/utar/assignment/Activity/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
@@ -35,7 +36,6 @@ public class MainActivity extends AppCompatActivity {
         bottomNav = findViewById(R.id.bottom_nav);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
-
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -54,9 +54,9 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.nav_friend:
                     selectedFragment = new FriendFragment();
                     break;
-                    case R.id.nav_profile:
-                selectedFragment = new ProfileFragment();
-                break;
+                 case R.id.nav_profile:
+                    selectedFragment = new ProfileFragment();
+                    break;
 
             }
 
